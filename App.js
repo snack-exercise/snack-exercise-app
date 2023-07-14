e5cb10c35db825a2f31dcf9a1182ad74c62f3b28
정희수 <heesoo@jeonghuisuui-MacBookPro.local>
2023-07-14 14:36:16 +0900

    fix : url 수정 #3

--- App.js
+++ App.js
@@ -5,8 +5,7 @@ import { WebView } from 'react-native-webview';
 export default function App() {
   return (
     <WebView
-      style={styles.container}
-      source={{ uri: 'https://snack-exercise.vercel.app' }}
+      source={{ uri: 'https://snackexercise.vercel.app' }}
     />
   );
 }
